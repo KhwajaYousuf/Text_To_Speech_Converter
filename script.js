6f9cc297dfc48623e07be3c4e72df8faf0aaa62a
Yousuf Mohiyuddeen <kyousufmo17@gmail.com>
2024-06-28 20:14:04 -0400

    Added event listener for voices changed on speech synthesis

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ let voices = [];
 
 let voiceSelect = document.querySelector("select");
 
+window.speechSynthesis.onvoiceschanged = () => {
+    
+}
+
 document.querySelector("button").addEventListener("click", () =>{
     speech.text =  document.querySelector("textarea").value;
     window.speechSynthesis.speak(speech);
